39199e10e61b1404d59adc73566d2cca4d77d403
MohamedNafis <mdnafis.mail@gmail.com>
2023-01-02 19:58:22 -0500

    Automation Framework Day04

--- src/main/java/common/CommonAction.java
+++ src/main/java/common/CommonAction.java
@@ -1,29 +1,36 @@
 package common;
 
-
 import java.util.NoSuchElementException;
 
 import org.openqa.selenium.WebElement;
 
 public class CommonAction {
-	
+
 	public static void clickElement(WebElement element) {
-		
-		try{
+
+		try {
 			element.click();
-		}
-		catch (NoSuchElementException | NullPointerException e) {
+		} catch (NoSuchElementException | NullPointerException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public static void inputText(WebElement element, String input) {
 		try {
 			element.sendKeys(input);
-		}
-		catch (NoSuchElementException | NullPointerException e) {
+		} catch (NoSuchElementException | NullPointerException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void clearText(WebElement element) {
+		try {
+			element.clear();
+		} catch (NoSuchElementException | NullPointerException e) {
+			e.printStackTrace();
+
+		}
+
 	}
 }
